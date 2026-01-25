5425472233af77a6d8ca0a3709a110315fd4f67e
Miltt4869 <trami2k4tm@gmail.com>
2026-01-25 17:40:09 +0700

    hoang qui dz

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
       proxy: {
         '/api': {
           // target: 'http://localhost:8081',
-          target: 'http://127.0.0.1:8081',
+          target: 'http://127.0.0.1:5000',
           changeOrigin: true,
           secure: false,
         },
